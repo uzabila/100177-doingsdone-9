04b1cc01ecffee3c9411865437dac05d9e17d603
uzabila <uzabila@yandex.ru>
2019-05-01 16:30:47 +0300

    Добавил файл для создания Базы Данных со всеми таблицами schema.sql

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE ermilov_doingsdone
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+reg_date DATE,
+name CHAR(255) NOT NULL,
+email CHAR(128) NOT NULL UNIQUE,
+password CHAR(64) NOT NULL
+);
+
+CREATE TABLE tasks (
+id INT AUTO_INCREMENT PRIMARY KEY,
+date_create DATETIME,
+date_exp DATE,
+name CHAR(255),
+status INT,
+task_url CHAR(255) NOT NULL UNIQUE,
+cat_id INT,
+user_id INT
+);
+
+CREATE TABLE projects_cat (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name CHAR(255) NOT NULL UNIQUE
+); 
